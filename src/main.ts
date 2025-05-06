9bd4075c4e9fd12a10f2b0fd6667ad6e05f96802
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-05-06 17:36:09 +0500

    ADD: установил библиотеку prime vue

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,20 @@ import "./assets/main.css";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 
+import Aura from "@primeuix/themes/aura";
+import PrimeVue from "primevue/config";
+
 import App from "./App.vue";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+
+app.use(pinia);
 
 app.mount("#app");
