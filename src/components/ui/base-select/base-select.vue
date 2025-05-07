ef60f9b8d9a8d1b1418c9674b5ae3c07f8702f69
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-05-07 13:49:22 +0500

    ADD: разработал компонент селкта

---
+++ src/components/ui/base-select/base-select.vue
@@ -0,0 +1,33 @@
+<template>
+  <float-label variant="on">
+    <p-select
+      v-model="selected"
+      :options="options"
+      :option-label="optionLabel"
+      style="width: 100%"
+    />
+
+    <label v-if="label" :for="id">
+      {{ label }}
+    </label>
+  </float-label>
+</template>
+
+<script setup lang="ts" generic="T">
+import FloatLabel from "primevue/floatlabel";
+import PSelect from "primevue/select";
+
+defineOptions({
+  name: "base-select",
+});
+
+defineProps<{
+  id: string;
+  label?: string;
+  placeholder?: string;
+  options: T[];
+  optionLabel: string;
+}>();
+
+const selected = defineModel<T>();
+</script>
