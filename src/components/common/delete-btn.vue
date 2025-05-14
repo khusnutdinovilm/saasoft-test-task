4c8ae0c6385f9e80d3c924716903bc658cee8e68
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-05-15 03:50:07 +0500

    ADD: разработал компонент кнопки для удаления записи

---
+++ src/components/common/delete-btn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="delete-btn">
+    <i class="pi pi-trash"></i>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "delete-btn",
+});
+</script>
+
+<style lang="scss">
+.delete-btn {
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+
+  border: 1px solid white;
+  border-radius: 4px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
